7e6d78b1b4b9a6a3bbcdab166f6c4bcac09bd098
Ankit Sahu <ankit@Ankits-MBP.hsd1.co.comcast.net>
2020-06-17 00:03:40 -0600

    added code for generating graphs

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # Titanic: Machine Learning from Disaster
-I will update this page as I progress
+## Train data characterstics
+titanic = pd.read_csv("train.csv")
+
+### display shape of titanic
+print(titanic.shape)
+(891, 12)
+
+### check if there are duplicate rows, it has 0 rows implying there are no dplicates
+dplicateRows = titanic[titanic.duplicated()]
+print(dplicateRows.shape)
+(0, 12)
+
+No duplicates found
+
+### check data types of each column
+print(titanic.dtypes)
+PassengerId      int64
+Survived         int64
+Pclass           int64
+Name            object
+Sex             object
+Age            float64
+SibSp            int64
+Parch            int64
+Ticket          object
+Fare           float64
+Cabin           object
+Embarked        object
+
+### #check for missing values
+print(titanic.isnull().sum())
+
+PassengerId      0
+Survived         0
+Pclass           0
+Name             0
+Sex              0
+Age            177
+SibSp            0
+Parch            0
+Ticket           0
+Fare             0
+Cabin          687
+Embarked         2
+
+Based on above, we can see there are lot of missing values in Age and Cabin. We need to address these in any model which we use.
+
+## Test data characterstics
+### read data from test.csv file into a pandas dataframe
+test_titanic = pd.read_csv("test.csv")
+
+### display shape of titanic
+print(test_titanic.shape)
+(418, 11)
+
+Notice that we have only 11 columns, we need to predict the survived column of this data set
+### check data types of each column
+print(test_titanic.dtypes)
+PassengerId      int64
+Pclass           int64
+Name            object
+Sex             object
+Age            float64
+SibSp            int64
+Parch            int64
+Ticket          object
+Fare           float64
+Cabin           object
+Embarked        object
+
+No anamaly dfound in data types of columns in train and test data
+
+### check if there are duplicate rows, it has 0 rows implying there are no dplicates
+dplicateRowsT = test_titanic[test_titanic.duplicated()]
+print(dplicateRowsT.shape)
+(0, 11)
+
+No duplicates found
--- titanic.py
+++ titanic.py
@@ -3,7 +3,219 @@
 """
 Created on Sun Jun 14 12:05:47 2020
 
-@author: ankit
+@author: Ankit Sahu
+
+Objective: To predict the Survival of passengers on Titanic ship based on passenger attributes.
+
 """
+
+#Import the required libraries
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import weasyprint as wsp
+
+pd.set_option('max_columns', 12)
+
+#change the working directory 
+os.chdir("/Users/ankit/CU/kaggle/titanic")
+
+#read data from test.csv file into a pandas dataframe
+titanic = pd.read_csv("train.csv")
+
+#display shape of titanic
+print(titanic.shape)
+
+#display 5 records
+print(titanic.head(5))
+
+#check data types of each column
+print(titanic.dtypes)
+
+#check if there are duplicate rows, it has 0 rows implying there are no dplicates
+dplicateRows = titanic[titanic.duplicated()]
+print(dplicateRows.shape)
+
+#checck for missing values
+print(titanic.isnull().sum())
+
+#now check the test data for similar things
+#read data from test.csv file into a pandas dataframe
+test_titanic = pd.read_csv("test.csv")
+
+#display shape of titanic
+print(test_titanic.shape)
+
+#display 5 records
+print(test_titanic.head(5))
+
+#check data types of each column
+print(test_titanic.dtypes)
+
+#check if there are duplicate rows, it has 0 rows implying there are no dplicates
+dplicateRowsT = test_titanic[test_titanic.duplicated()]
+print(dplicateRowsT.shape)
+
+#checck for missing values
+print(test_titanic.isnull().sum())
+
+
+#plot graphs
+fig = plt.figure(figsize=(18,6))  
+
+#normalized % of survival rate
+plt.figure(0)
+plt.title("Survival distribution")
+titanic.Survived.value_counts(normalize=True).plot(kind="bar",alpha=0.5)
+
+#normalized plot of survival 
+plt.figure(1)
+plt.title("Passenger Class distribution")
+titanic.Pclass.value_counts(normalize=True).plot(kind="bar",alpha=0.5)
+
+#scatter plot of survival wrt Age
+plt.figure(2)
+plt.title("Age distribution")
+plt.scatter(titanic.Survived, titanic.Age, alpha=0.1)
+
+#scatter plot of survival wrt Sex
+plt.figure(3)
+plt.title("Gender distribution")
+plt.scatter(titanic.Survived, titanic.Sex, alpha=0.1)
+
+#line chart of age wrt to class
+plt.figure(4)
+for x in [1,2,3]:    ## for 3 classes
+    titanic.Age[titanic.Pclass == x].plot(kind="kde")
+plt.title("Age wrt Pclass")
+plt.legend(("1st","2nd","3rd"))
+
+#normalized plot of Embarked 
+plt.figure(5)
+plt.title("Embarked  distribution")
+titanic.Embarked.value_counts(normalize=True).plot(kind="bar",alpha=0.5)
+
+plt.figure(6)
+#histogram of all variables
+titanic.hist(bins=10,figsize=(9,7),grid=False);
+
+#hist of sex, age and survived 
+plt.figure(7)
+g = sns.FacetGrid(titanic, col="Sex", row="Survived", margin_titles=True)
+g.map(plt.hist, "Age",color="purple");
+
+#hist of survived, sex and Pclass
+plt.figure(8)
+g = sns.FacetGrid(titanic, col="Sex", row="Survived", margin_titles=True)
+g.map(plt.hist, "Pclass",color="purple");
+
+#hist of pclass and survived fare and age
+plt.figure(9)
+g = sns.FacetGrid(titanic, hue="Survived", col="Pclass", margin_titles=True,                  palette={1:"seagreen", 0:"gray"})
+g=g.map(plt.scatter, "Fare", "Age",edgecolor="w").add_legend();
+
+#hist of fare, age, sex and survived
+plt.figure(10)
+g = sns.FacetGrid(titanic, hue="Survived", col="Sex", margin_titles=True,
+                palette="Set1",hue_kws=dict(marker=["^", "v"]))
+g.map(plt.scatter, "Fare", "Age",edgecolor="w").add_legend()
+plt.subplots_adjust(top=0.8)
+g.fig.suptitle('Survival by Gender , Age and Fare');
+
+#BAR PLOT OF passengers per location
+plt.figure(11)
+titanic.Embarked.value_counts().plot(kind='bar', alpha=0.55)
+plt.title("Passengers per boarding location");
+
+plt.figure(12)
+#factor plor of survived per location
+sns.factorplot(x = 'Embarked',y="Survived", data = titanic,color="b");
+
+plt.figure(13)
+#factorplot for survived based on Pclass
+sns.factorplot(x = 'Pclass',y="Survived", data = titanic,color="b");
+
+plt.figure(14)
+#factor plot for survived per Sex
+sns.factorplot(x = 'Sex',y="Survived", data = titanic,color="b");
+
+plt.figure(15)
+#factorplot for survived per sibsp
+sns.factorplot(x = 'SibSp',y="Survived", data = titanic,color="b");
+
+plt.figure(16)
+#factor plot for survived per Parch
+sns.factorplot(x = 'Parch',y="Survived", data = titanic,color="b");
+
+
+#How many Men and Women Survived by Passenger Class
+plt.figure(17)
+sns.set(font_scale=1)
+g = sns.factorplot(x="Sex", y="Survived", col="Pclass",
+                    data=titanic, saturation=.5,
+                    kind="bar", ci=None, aspect=.6)
+(g.set_axis_labels("", "Survival Rate")
+    .set_xticklabels(["Men", "Women"])
+    .set_titles("{col_name} {col_var}")
+    .set(ylim=(0, 1))
+    .despine(left=True))  
+plt.subplots_adjust(top=0.8)
+g.fig.suptitle('How many Men and Women Survived by Passenger Class');
+
+#Survival distribution by age
+plt.figure(18)
+ax = sns.boxplot(x="Survived", y="Age", 
+                data=titanic)
+plt.figure(19)
+ax = sns.stripplot(x="Survived", y="Age",
+                   data=titanic, jitter=True,
+                   edgecolor="gray")
+plt.title("Survival by Age",fontsize=12);
+
+#age distribution by class
+plt.figure(20)
+titanic.Age[titanic.Pclass == 1].plot(kind='kde')    
+titanic.Age[titanic.Pclass == 2].plot(kind='kde')
+titanic.Age[titanic.Pclass == 3].plot(kind='kde')
+ # plots an axis lable
+plt.xlabel("Age")    
+plt.title("Age Distribution within classes")
+# sets our legend for our graph.
+plt.legend(('1st Class', '2nd Class','3rd Class'),loc='best') ;
+
+
+#corrrelation plot
+plt.figure(21)
+corr=titanic.corr()#["Survived"]
+plt.figure(figsize=(10, 10))
+
+sns.heatmap(corr, vmax=.8, linewidths=0.01,
+            square=True,annot=True,cmap='YlGnBu',linecolor="white")
+plt.title('Correlation between features');
+
+#correlation of features with target variable
+titanic.corr()["Survived"]
+
+plt.figure(22)
+g = sns.factorplot(x="Age", y="Embarked",
+                    hue="Sex", row="Pclass",
+                    data=titanic[titanic.Embarked.notnull()],
+                    orient="h", size=2, aspect=3.5, 
+                   palette={'male':"purple", 'female':"blue"},
+                    kind="violin", split=True, cut=0, bw=.2);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
